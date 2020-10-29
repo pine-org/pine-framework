10f69f69a9dc87ae7b42f2fe225591889bf44094
samanalishiri <samanalishiri@gmail.com>
2020-10-29 22:04:02 +0100

    improve UI

--- e-shop-ui/src/app/home/home.component.ts
+++ e-shop-ui/src/app/home/home.component.ts
@@ -20,17 +20,16 @@ export class HomeComponent implements OnInit {
     return "GOODS";
   }
 
-  getColumns(): Properties[] {
-    return [
-      Properties.builder('ID').field("id").build(),
-      Properties.builder('Name').field("name").build(),
-      Properties.builder('Code').field("code").build(),
-      Properties.builder('Price').field("price").build(),
-      Properties.builder('Description').field("description").build(),
-    ];
-  }
+  columns = [
+    Properties.builder('ID').field("id").build(),
+    Properties.builder('Name').field("name").build(),
+    Properties.builder('Code').field("code").build(),
+    Properties.builder('Price').field("price").build(),
+    Properties.builder('Description').field("description").build(),
+  ];
 
   getService(): Service<any> {
     return this.service;
   }
+
 }
