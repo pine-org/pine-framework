2c518e97518c5bf832b3add82dbd712f776fd429
samanalishiri <samanalishiri@gmail.com>
2020-10-29 09:36:02 +0100

    improve i18n

--- spring-core/spring-restapi/src/main/java/com/pineframework/core/spring/restapi/config/I18nConfig.java
+++ spring-core/spring-restapi/src/main/java/com/pineframework/core/spring/restapi/config/I18nConfig.java
@@ -1,11 +1,47 @@
 package com.pineframework.core.spring.restapi.config;
 
+import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
+import org.springframework.context.annotation.Profile;
 import org.springframework.context.annotation.PropertySource;
+import org.springframework.web.servlet.LocaleResolver;
+import org.springframework.web.servlet.config.annotation.InterceptorRegistry;
+import org.springframework.web.servlet.config.annotation.WebMvcConfigurer;
+import org.springframework.web.servlet.i18n.LocaleChangeInterceptor;
+import org.springframework.web.servlet.i18n.SessionLocaleResolver;
+
+import java.util.Locale;
 
 @Configuration
 @PropertySource("classpath:messages.properties")
 @PropertySource("classpath:messages_fa.properties")
-public class I18nConfig {
+public class I18nConfig implements WebMvcConfigurer {
 
+    @Bean
+    @Profile("US")
+    public LocaleResolver usaLocaleResolver() {
+        SessionLocaleResolver slr = new SessionLocaleResolver();
+        slr.setDefaultLocale(Locale.US);
+        return slr;
+    }
+
+    @Bean
+    @Profile("IR")
+    public LocaleResolver iranLocaleResolver() {
+        SessionLocaleResolver slr = new SessionLocaleResolver();
+        slr.setDefaultLocale(new Locale("fa", "IR"));
+        return slr;
+    }
+
+    @Bean
+    public LocaleChangeInterceptor localeChangeInterceptor() {
+        LocaleChangeInterceptor lci = new LocaleChangeInterceptor();
+        lci.setParamName("lang");
+        return lci;
+    }
+
+    @Override
+    public void addInterceptors(InterceptorRegistry registry) {
+        registry.addInterceptor(localeChangeInterceptor());
+    }
 }
